1d2923130b0b9a8990404b7508f37d050b377bd8
Adem-hub <57374875+Adem-hub@users.noreply.github.com>
2021-01-09 11:22:23 +0100

    Add files via upload

--- ProjetAppreciations.py
+++ ProjetAppreciations.py
@@ -3,32 +3,33 @@ Liste_Appreciations={"Trimestre très moyen":[0,4],"Trimestre moyen":[4,8],"Trim
 Liste_Regularite={"un parcours trop irregulier":[-10,-1],"a été régulier tout au long":[-1,1],"une progression remarquable":[1,10]}
 
 
-#Essayez avec Jules=Eleve([12,17,13,14]) par exemple. self.Tot represente l'appreciation totale Les virgules etc ne sont pas encore au point. Il reste quelques modifs.
+#Essayez avec Jules=Eleve([14,17,18,12]) par exemple
+
 
 class Eleve:
     def __init__(self,ListeNotes):
         self.ListeNotes=ListeNotes
         self.Moyenne_Eleve=sum(ListeNotes)/len(ListeNotes)
-        self.Appreciation=''
-        self.Regularite=''
-        self.Appreciation_()
-        self.Regularite_()
+        self.Appreciation_1=''
+        self.Appreciation_2=''
+        self.Appreciation()
+        self.Regularite()
         self.Incoherence()
 
-        self.Tot= self.Appreciation+' '+self.Regularite
+        self.App_Finale= self.Appreciation_1+' '+self.Appreciation_2
 
     def Incoherence(self):
         #Il y a peut être d'autres incoherences à ajouter, je n'en ai pas trouvé pour l'instant
         if self.Moyenne_Eleve<10:
-            if self.Regularite=="a été régulier tout au long" or self.Regularite=="une progression remarquable":
-                self.Regularite=='Vous pouvez progresser, vous en avez les capacités.'
+            if self.Appreciation_2=="a été régulier tout au long" or self.Appreciation_2=="une progression remarquable":
+                self.Appreciation_2=='Vous pouvez progresser, vous en avez les capacités.'
 
-    def Appreciation_(self):
+    def Appreciation(self):
         for App in Liste_Appreciations:
             if Liste_Appreciations[App][0]<=self.Moyenne_Eleve<Liste_Appreciations[App][1]:
-                self.Appreciation = App
+                self.Appreciation_1 = App
 
-    def Regularite_(self):
+    def Regularite(self):
         L=[]
         for i in range(len(self.ListeNotes)-1):
             Taux= ((self.ListeNotes[i+1]-self.ListeNotes[i])/2)
@@ -36,16 +37,14 @@ class Eleve:
         #Ici on essaye de voir si le parcours de l'eleve est en dents de scie ou pas
         Parcours_Febrile=[n for n in L if n>4]
         Parcours_Febrile2=[n for n in L if n<-4]
-        print(Parcours_Febrile,Parcours_Febrile2)
-        print(L)
         if len(Parcours_Febrile)>=2 and len(Parcours_Febrile2)>=2:
-            self.Regularite=" cependant dommage, vous avez manqué d'assiduité! Les résultats sont beacoup trop en dents de scie"
+            self.Appreciation_2=" cependant dommage, vous avez manqué d'assiduité! Les résultats sont beacoup trop en dents de scie"
         else:
         #S'il n'est pas en dents de scie, alors on lui donne une appreciation adéquate
             somme=sum(L)
             for Reg in Liste_Regularite:
                 if Liste_Regularite[Reg][0]<=somme<Liste_Regularite[Reg][1]:
-                    self.Regularite=Reg
+                    self.Appreciation_2=Reg
 
 
 
